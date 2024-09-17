d3bd36296d1765892717a12a3c3d2af386e29f4c
Dmytro Vynnyk <simbiatoff@gmail.com>
2024-09-17 12:12:27 +0300

    Add tx validations

--- src/utils/deploy.ts
+++ src/utils/deploy.ts
@@ -1,3 +1,4 @@
+import { CLPublicKey, decodeBase16, DeployUtil } from 'casper-js-sdk';
 import {
   IAssociatedKeysDeploy,
   IAuctionDeploy,
@@ -8,8 +9,15 @@ import {
   INftDeploy,
 } from '../domain';
 import { Maybe } from '../typings';
-import { Deploy } from 'casper-js-sdk/dist/lib/DeployUtil';
-import { TransactionV1 } from 'casper-js-sdk/dist/lib/TransactionUtil';
+import { arrayEquals, Deploy } from 'casper-js-sdk/dist/lib/DeployUtil';
+import {
+  serializeBody,
+  serializeHeader,
+  TransactionV1,
+} from 'casper-js-sdk/dist/lib/TransactionUtil';
+import { byteHash } from 'casper-js-sdk/dist/lib/ByteConverters';
+import { validateSignature } from 'casper-js-sdk/dist/lib/Keys';
+import { TypedJSON } from 'typedjson';
 
 export const isAuctionDeploy = (deploy: IDeploy): deploy is IAuctionDeploy => {
   return deploy.type === 'AUCTION';
@@ -44,34 +52,88 @@ export const isContractCallExecutionType = (deploy: IDeploy) =>
   Number(deploy.executionTypeId) > 1 && Number(deploy.executionTypeId) < 6;
 export const isTransferExecutionType = (deploy: IDeploy) => Number(deploy.executionTypeId) === 6;
 
-export const getDeployFromJson = (json: string): Maybe<Deploy> => {
+export const getDeployFromJson = (json: string | Record<string, any>): Maybe<Deploy> => {
   try {
-    const data: Record<string, any> = JSON.parse(json);
+    const data: Record<string, any> = typeof json === 'string' ? JSON.parse(json) : json;
 
-    const deploy: Maybe<Deploy> =
+    const deployJson: Maybe<Record<string, any>> =
       data.deploy ?? data.Deploy ?? data?.transaction?.Deploy ?? data ?? null;
 
-    if (deploy?.hash && deploy?.header?.account) {
-      return deploy;
+    if (!(deployJson?.hash && deployJson?.header?.account)) {
+      return null;
     }
 
-    return null;
+    const res = DeployUtil.deployFromJson({ deploy: deployJson });
+
+    return res.ok ? res.val : null;
   } catch (e) {
+    console.log('-------- e', e);
     return null;
   }
 };
 
-export const getTransactionV1FromJson = (json: string): Maybe<TransactionV1> => {
+export const getTransactionV1FromJson = (
+  json: string | Record<string, any>,
+): Maybe<TransactionV1> => {
   try {
-    const data: Record<string, any> = JSON.parse(json);
-    const tx: Maybe<TransactionV1> = data?.transaction?.Version1 ?? data?.Version1 ?? data ?? null;
+    const data: Record<string, any> = typeof json === 'string' ? JSON.parse(json) : json;
+    const tx: Maybe<Record<string, any>> =
+      data?.transaction?.Version1 ?? data?.Version1 ?? data ?? null;
 
-    if (tx?.hash && tx?.header?.initiatorAddr) {
-      return tx;
+    if (!(tx?.hash && tx?.header?.initiator_addr)) {
+      return null;
     }
 
-    return null;
+    return transactionV1FromJson(tx);
   } catch (e) {
+    console.log('-------- e', e);
     return null;
   }
+};
+
+export const validateTransactionV1 = (tx: TransactionV1): TransactionV1 => {
+  const serializedBody = serializeBody(tx.body);
+  const bodyHash = byteHash(serializedBody);
+
+  if (!arrayEquals(tx.header.bodyHash, bodyHash)) {
+    throw new Error(`Invalid tx: bodyHash mismatch. Expected: ${bodyHash},
+                  got: ${tx.header.bodyHash}.`);
+  }
+
+  const serializedHeader = serializeHeader(tx.header).unwrap();
+  const txHash = byteHash(serializedHeader);
+
+  if (!arrayEquals(tx.hash, txHash)) {
+    throw new Error(`Invalid tx: hash mismatch. Expected: ${txHash},
+                  got: ${tx.hash}.`);
+  }
+
+  const isProperlySigned = tx.approvals.every(({ signer, signature }) => {
+    const pk = CLPublicKey.fromFormattedString(signer, false);
+    const signatureRaw = decodeBase16(signature.slice(2));
+    return validateSignature(tx.hash, signatureRaw, pk);
+  });
+
+  if (!isProperlySigned) {
+    throw new Error('Invalid signature.');
+  } else {
+    return tx;
+  }
+};
+
+export const transactionV1FromJson = (json: Record<string, any>): TransactionV1 => {
+  let tx = null;
+
+  try {
+    const serializer = new TypedJSON(TransactionV1);
+    tx = serializer.parse(json);
+  } catch (serializationError) {
+    throw new Error(`${serializationError}`);
+  }
+
+  if (!tx) {
+    throw new Error("The JSON can't be parsed as a TransactionV1.");
+  }
+
+  return validateTransactionV1(tx);
 };
