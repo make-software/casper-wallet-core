59f42fc55c0e685b95378cc4d46b20a5a9c7fe71
Dmytro Vynnyk <simbiatoff@gmail.com>
2025-02-11 15:55:38 +0100

    Add integration network

--- src/domain/common/common.ts
+++ src/domain/common/common.ts
@@ -2,7 +2,7 @@ export interface IEntity {
   readonly id: string;
 }
 
-export type CasperNetwork = 'mainnet' | 'testnet' | 'devnet';
+export type CasperNetwork = 'mainnet' | 'testnet' | 'devnet' | 'integration';
 export type Network = CasperNetwork;
 export type AvailableLanguageTags = 'en';
 export type SupportedFiatCurrencies = 'USD';
