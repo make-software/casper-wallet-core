15917231e5226be59ce6c1789bf1aa29094e5a03
Dmytro Vynnyk <simbiatoff@gmail.com>
2025-07-10 12:56:58 +0200

    Add washHash calculation using blake2b for Wasm and WasmProxy actions

--- src/data/dto/txSignatureRequest/actions/wasmAction.ts
+++ src/data/dto/txSignatureRequest/actions/wasmAction.ts
@@ -1,10 +1,17 @@
-import { Transaction } from 'casper-js-sdk';
+import { Conversions, Transaction } from 'casper-js-sdk';
 import { IAccountInfo, ITxSignatureRequestWasmAction } from '../../../../domain';
 import { getTxSignatureRequestArgs } from './argumentsParser';
+import { blake2b } from '@noble/hashes/blake2';
 
 export function getTxSignatureRequestWasmAction(
   tx: Transaction,
   accountInfoMap: Record<string, IAccountInfo> = {},
 ): ITxSignatureRequestWasmAction {
-  return { type: 'WASM', args: getTxSignatureRequestArgs(tx.args, accountInfoMap, tx.chainName) };
+  return {
+    type: 'WASM',
+    args: getTxSignatureRequestArgs(tx.args, accountInfoMap, tx.chainName),
+    washHash: Conversions.encodeBase16(
+      blake2b(tx.target.session?.moduleBytes ?? new Uint8Array(), { dkLen: 32 }),
+    ),
+  };
 }
